62ab7c66a9f5d6c7d0bcb0514d78c37e8f1505ca
Muhammad Bagas Pradana <64050579+Bagas8015@users.noreply.github.com>
2020-11-09 18:06:15 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,95 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+
+if (workbox){
+    workbox.precaching.precacheAndRoute([
+        { url: '/', revision: '1' },
+        { url: '/nav.html', revision: '1' },
+        { url: '/index.html', revision: '1' },
+        { url: '/detail_info.html', revision: '1' },
+        { url: '/manifest.json', revision: '1' },
+        { url: '/push.js', revision: '1' },
+        { url: '/pages/favorite.html', revision: '1' },
+        { url: '/pages/information.html', revision: '1' },
+        { url: '/pages/klasemen.html', revision: '1' },
+        { url: '/css/materialize.min.css', revision: '1' },
+        { url: '/js/materialize.min.js', revision: '1' },
+        { url: '/js/api.js', revision: '1' },
+        { url: '/js/db.js', revision: '1' },
+        { url: '/js/idb.js', revision: '1' },
+        { url: '/js/nav.js', revision: '1' },
+        { url: '/js/register_sw.js', revision: '1' },
+        { url: '/js/detail_info.js', revision: '1' },
+        { url: '/icon/icon-48new.png', revision: '1' },
+        { url: '/icon/icon-96new.png', revision: '1' },
+        { url: '/icon/icon-192new.png', revision: '1' },
+        { url: '/icon/icon-512new.png', revision: '1' },
+        { url: '/icon/maskable_icon_new.png', revision: '1' },
+    ]);
+
+    workbox.routing.registerRoute(
+      new RegExp('https://api.football-data.org/v2/'),
+      workbox.strategies.staleWhileRevalidate()
+    );
+
+    workbox.routing.registerRoute(
+      /.*(?:googleapis|gstatic)\.com/,
+      workbox.strategies.staleWhileRevalidate({
+        cacheName: 'source-google-font',
+      })
+    );
+
+    workbox.routing.registerRoute(
+      new RegExp('/pages/'),
+      workbox.strategies.staleWhileRevalidate({
+          cacheName: 'pages',
+      })
+    );
+
+    workbox.routing.registerRoute(
+      /\.(?:js|css)$/,
+      workbox.strategies.cacheFirst({
+          cacheName: 'js-css',
+      })
+  );
+
+  workbox.routing.registerRoute(
+    /.*(?:png|gif|jpg|jpeg|svg|ico)$/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'icon',
+        plugins: [
+        new workbox.cacheableResponse.Plugin({
+            statuses: [0, 200]
+        }),
+        new workbox.expiration.Plugin({
+            maxEntries: 100,
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+        ]
+    })
+  );
+  console.log(`Workbox berhasil dimuat`);
+}else
+  console.log(`Workbox gagal dimuat`);
+
+//new line push event API
+self.addEventListener('push', function(event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = 'Push message no payload';
+  }
+  var options = {
+    body: body,
+    icon: 'icon/icon-192new.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
+
